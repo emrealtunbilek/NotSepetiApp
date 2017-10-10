459397870a92d0ee334b9c16c350986d2b8ef9fb
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-10 22:58:06 +0300

    Toolbar eklendi ve arka plan resmi atandı

--- app/src/main/java/emrealtunbilek/com/notsepetiapp/ActivityMain.java
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/ActivityMain.java
@@ -2,12 +2,19 @@ package emrealtunbilek.com.notsepetiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bos_liste);
+        setContentView(R.layout.activity_main);
+        mToolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+
     }
 }
