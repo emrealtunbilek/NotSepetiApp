9731ef940b0920722ca3975d3bac57106a0bd566
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-11 01:21:18 +0300

    Dialog layoutumuz tamamlandı
    Butonlara click işlemi yapıldı

--- app/src/main/java/emrealtunbilek/com/notsepetiapp/FragmentDialogYeniNot.java
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/FragmentDialogYeniNot.java
@@ -6,6 +6,11 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by Emre Altunbilek on 10.10.2017.
@@ -13,11 +18,37 @@ import android.view.ViewGroup;
 
 public class FragmentDialogYeniNot extends DialogFragment {
 
+    private ImageButton mBtnKapat;
+    private EditText mNotIcerik;
+    private DatePicker mNotTarih;
+    private Button mBtnNotEkle;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dialog_yeni_not, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBtnKapat= (ImageButton) view.findViewById(R.id.btn_dialog_kapat);
+        mNotIcerik=(EditText)view.findViewById(R.id.et_not);
+        mNotTarih= (DatePicker) view.findViewById(R.id.dp_tarih);
+        mBtnNotEkle= (Button) view.findViewById(R.id.btn_not_ekle);
 
+        mBtnKapat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        mBtnNotEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Not Eklendi", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
