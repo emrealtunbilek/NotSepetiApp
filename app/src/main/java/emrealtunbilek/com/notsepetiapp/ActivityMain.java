285874c8fa0de01d91a5aa0b3952e6957866a129
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-10 21:04:22 +0300

    Buton ve logo eklendi

---
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/ActivityMain.java
@@ -0,0 +1,13 @@
+package emrealtunbilek.com.notsepetiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bos_liste);
+    }
+}
