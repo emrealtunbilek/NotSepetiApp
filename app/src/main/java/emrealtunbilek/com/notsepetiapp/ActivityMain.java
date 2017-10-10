a8c006e0c47377fea1891861a6be7c012089db71
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-10 23:30:58 +0300

    Glide eklendi
    backgroun resmi glide ile yerleştirildi

--- app/src/main/java/emrealtunbilek/com/notsepetiapp/ActivityMain.java
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/ActivityMain.java
@@ -3,6 +3,10 @@ package emrealtunbilek.com.notsepetiapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -14,7 +18,17 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar= (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        backgrounResminiYerlestir();
 
+
+    }
+
+    private void backgrounResminiYerlestir() {
+        ImageView background= (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .apply(new RequestOptions().centerCrop())
+                .into(background);
 
     }
 }
