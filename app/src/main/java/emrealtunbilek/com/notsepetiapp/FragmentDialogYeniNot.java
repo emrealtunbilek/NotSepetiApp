1deceb06a4d701ea09a0fddf7d322249ed9f431e
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-12 00:35:52 +0300

    content provider olusturuldu
    insert methodu olusturuldu, çalışıyor

--- app/src/main/java/emrealtunbilek/com/notsepetiapp/FragmentDialogYeniNot.java
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/FragmentDialogYeniNot.java
@@ -1,5 +1,7 @@
 package emrealtunbilek.com.notsepetiapp;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -12,11 +14,15 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import emrealtunbilek.com.notsepetiapp.data.NotlarProvider;
+
 /**
  * Created by Emre Altunbilek on 10.10.2017.
  */
 
 public class FragmentDialogYeniNot extends DialogFragment {
+
+    static final Uri CONTENT_URI = NotlarProvider.CONTENT_URI;
 
     private ImageButton mBtnKapat;
     private EditText mNotIcerik;
@@ -32,10 +38,10 @@ public class FragmentDialogYeniNot extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mBtnKapat= (ImageButton) view.findViewById(R.id.btn_dialog_kapat);
-        mNotIcerik=(EditText)view.findViewById(R.id.et_not);
-        mNotTarih= (DatePicker) view.findViewById(R.id.dp_tarih);
-        mBtnNotEkle= (Button) view.findViewById(R.id.btn_not_ekle);
+        mBtnKapat = (ImageButton) view.findViewById(R.id.btn_dialog_kapat);
+        mNotIcerik = (EditText) view.findViewById(R.id.et_not);
+        mNotTarih = (DatePicker) view.findViewById(R.id.dp_tarih);
+        mBtnNotEkle = (Button) view.findViewById(R.id.btn_not_ekle);
 
         mBtnKapat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +53,11 @@ public class FragmentDialogYeniNot extends DialogFragment {
         mBtnNotEkle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Not Eklendi", Toast.LENGTH_LONG).show();
+                ContentValues values = new ContentValues();
+                values.put("notIcerik", mNotIcerik.getText().toString());
+                values.put("notTarih", mNotIcerik.getText().toString());
+                Uri uri = getActivity().getContentResolver().insert(CONTENT_URI, values);
+                Toast.makeText(getContext(), "" + uri, Toast.LENGTH_LONG).show();
             }
         });
     }
