edcd0ff07ec8c6a5542940ebbc8885c90a6e4c0f
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-22 03:23:33 +0300

    tıklama sınırları belirlendi

--- app/src/main/java/emrealtunbilek/com/notsepetiapp/TarihSecDatePicker.java
+++ app/src/main/java/emrealtunbilek/com/notsepetiapp/TarihSecDatePicker.java
@@ -1,21 +1,29 @@
 package emrealtunbilek.com.notsepetiapp;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.EventLog;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.EventListener;
 
 /**
  * Created by Emre Altunbilek on 21.10.2017.
  */
 
-public class TarihSecDatePicker extends LinearLayout{
+public class TarihSecDatePicker extends LinearLayout implements View.OnTouchListener {
 
     TextView mTextGun;
     TextView mTextAy;
@@ -39,34 +47,145 @@ public class TarihSecDatePicker extends LinearLayout{
     }
 
     private void init(Context context) {
-        View view=LayoutInflater.from(context).inflate(R.layout.tarih_view, this);
-        mCalendar=Calendar.getInstance();
-        mFormatter=new SimpleDateFormat("MMM");
+        View view = LayoutInflater.from(context).inflate(R.layout.tarih_view, this);
+        mCalendar = Calendar.getInstance();
+        mFormatter = new SimpleDateFormat("MMM");
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        mTextGun= (TextView) this.findViewById(R.id.tv_tarih_gun);
-        mTextAy= (TextView) this.findViewById(R.id.tv_tarih_ay);
-        mTextYil= (TextView) this.findViewById(R.id.tv_tarih_yil);
+        mTextGun = (TextView) this.findViewById(R.id.tv_tarih_gun);
+        mTextAy = (TextView) this.findViewById(R.id.tv_tarih_ay);
+        mTextYil = (TextView) this.findViewById(R.id.tv_tarih_yil);
 
-        int gun=mCalendar.get(Calendar.DATE);
-        int ay=mCalendar.get(Calendar.MONTH);
-        int yil=mCalendar.get(Calendar.YEAR);
+        mTextGun.setOnTouchListener(this);
+        mTextAy.setOnTouchListener(this);
+        mTextYil.setOnTouchListener(this);
 
-        guncelle(gun, ay, yil,0,0,0);
+        int gun = mCalendar.get(Calendar.DATE);
+        int ay = mCalendar.get(Calendar.MONTH);
+        int yil = mCalendar.get(Calendar.YEAR);
+
+        guncelle(gun, ay, yil, 0, 0, 0);
     }
 
-    private void guncelle(int gun, int ay, int yil, int saat, int dakika, int saniye){
+    private void guncelle(int gun, int ay, int yil, int saat, int dakika, int saniye) {
 
-        mTextGun.setText(""+gun);
+        mTextGun.setText("" + gun);
         mTextAy.setText(mFormatter.format(mCalendar.getTime()));
-        mTextYil.setText(""+yil);
+        mTextYil.setText("" + yil);
 
     }
 
-    public long getTime(){
-        return  mCalendar.getTimeInMillis();
+    public long getTime() {
+        return mCalendar.getTimeInMillis();
     }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch (v.getId()) {
+
+
+            case R.id.tv_tarih_gun:
+                processEventFor(mTextGun, event);
+                break;
+
+            case R.id.tv_tarih_ay:
+                processEventFor(mTextAy, event);
+                break;
+
+            case R.id.tv_tarih_yil:
+                processEventFor(mTextYil, event);
+                break;
+
+        }
+
+
+        return true;
+    }
+
+    private void processEventFor(TextView textView, MotionEvent event) {
+/*
+*
+* SOL - 0
+* YUKARIDA - 1
+* SAG - 2
+* AŞAĞI - 3
+*
+* */
+        Drawable[] drawables = textView.getCompoundDrawables();
+
+        if (yukariDrawableVarmi(drawables) && asagiDrawableVarmi(drawables)) {
+
+
+            Rect yukariSinir = drawables[1].getBounds();
+            Rect asagiSinir = drawables[3].getBounds();
+
+            float x=event.getX();
+            float y=event.getY();
+
+            if(yukariDrawableTiklandi(textView, yukariSinir, x, y)){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    Toast.makeText(getContext(), "Yukarı tıklandı : "+textView.getId(), Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }else if(asagiDrawableTiklandi(textView, asagiSinir, x, y)){
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    Toast.makeText(getContext(), "Aşağı tıklandı :" + textView.getId(), Toast.LENGTH_SHORT).show();
+                }
+
+            }else{
+
+
+            }
+
+
+        }
+
+    }
+
+
+
+    private boolean yukariDrawableTiklandi(TextView textView, Rect yukariSinir, float x, float y) {
+
+        int xmin=textView.getPaddingLeft();
+        int xmax=textView.getWidth() - textView.getPaddingRight();
+
+        int ymin=textView.getPaddingTop();
+        int ymax=textView.getPaddingTop()+ yukariSinir.height();
+
+        return x > xmin  && x < xmax && y > ymin && y<ymax;
+
+    }
+
+    private boolean asagiDrawableTiklandi(TextView textView, Rect asagiSinir, float x, float y) {
+
+        int xmin=textView.getPaddingLeft();
+        int xmax=textView.getWidth() - textView.getPaddingRight();
+
+        int ymax=textView.getHeight()- textView.getPaddingBottom();
+        int ymin=ymax-asagiSinir.height();
+
+
+        return x > xmin  && x < xmax && y > ymin && y<ymax;
+    }
+
+    private boolean yukariDrawableVarmi(Drawable[] drawables) {
+
+        return drawables[1] != null;
+    }
+
+    private boolean asagiDrawableVarmi(Drawable[] drawables) {
+
+        return drawables[3] != null;
+    }
+
+
 }
